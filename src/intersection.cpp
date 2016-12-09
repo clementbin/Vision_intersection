152c2b171868545dcc1d08318bc9f3de67388311
DALLEN Louis-marie <ldallen@duran.pedago.ipb.fr>
2016-12-09 13:57:04 +0100

    work on pattern detection

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <list>
 
 #include <opencv2/opencv.hpp>
 #include "opencv2/highgui/highgui.hpp"
@@ -17,44 +18,83 @@ using namespace std;
 #define iLowVGREEN 40//limit = 40
 #define iHighVGREEN 175//limit = 175
 String imageNames[16] = {"001-rgb.png","0001-rgb.png","34-rgb.png","074-rgb.png","083-rgb.png","094-rgb.png","099-rgb.png","101-rgb.png","147-rgb.png","156-rgb.png","157-rgb.png","164-rgb.png","194-rgb.png","205-rgb.png","264-rgb.png","268-rgb.png"};
+String templates[3] = {"corner_pattern.png","cross_pattern.png","t_pattern.png"};
 
-
-void matchingMethod( int,Mat img,Mat templ )
+void matchingMethod( int,Mat img )
 {
+  
+  vector<Mat> results;
+  
+  int match_method = 5; // Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED
+  
+  Mat img_display,templ;
+  
+  templ = imread("../data/"+templates[0],0);
+ 
+  for(int i=0; i<3; i++){
   /// Source image to display
-  Mat img_display,result;
+    Mat result;
   img.copyTo( img_display );
-
+  templ = imread("../data/"+templates[i],0);
   /// Create the result matrix
   int result_cols =  img.cols - templ.cols + 1;
   int result_rows = img.rows - templ.rows + 1;
 
-  result.create( result_rows, result_cols, 0 );
+   result.create( result_rows, result_cols, 0 );
+  
 
-  // Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED
-  int match_method = 3;
   /// Do the Matching and Normalize
+
   matchTemplate( img, templ, result, match_method );
+
+  results.push_back(result);
+  }
+
+  Mat result = results[0].clone();
+  Mat featureLoc(result.rows, result.cols, 0, Scalar(0)); 
+
+  for (int n = 1;n<3;n++){
+      for (int i = 0; i<result.rows;i++){
+	for (int j = 0;j<result.cols;j++){
+	  if(results[n].at<float>(i,j)>result.at<float>(i,j)){
+	    result.at<float>(i,j) = results[n].at<float>(i,j);
+	    featureLoc.at<uchar>(i,j) = n;
+	      }
+	}
+      }
+    }
+
   normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
 
-  /// Localizing the best match with minMaxLoc
-  double minVal; double maxVal; Point minLoc; Point maxLoc;
-  Point matchLoc;
+    
+  vector<Vec4i> maxPoints;
+  for (int i = 0; i<result.rows;i++){
+    for (int j = 0;j<result.cols;j++){
+      if (result.at<float>(i,j)>0.99){
+	maxPoints.push_back(Vec4i(i,j,result.at<float>(i,j),featureLoc.at<uchar>(i,j)));
+      }
+    }
+    }
+    cout << maxPoints.size() << endl;
 
-  minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+  // /// Localizing the best match with minMaxLoc
+  // double minVal; double maxVal; Point minLoc; Point maxLoc;
+  // Point matchLoc;
 
-  /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
-  if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
-    { matchLoc = minLoc; }
-  else
-    { matchLoc = maxLoc; }
-
+  // minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );
+  
+  // /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+  // if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
+  //   { matchLoc = minLoc; }
+  // else
+  //   { matchLoc = maxLoc; }
+  
   /// Show me what you got
-  rectangle( img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
-  rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
+  // rectangle( img_display, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar(128), 2, 8, 0 );
+  //rectangle( result, matchLoc, Point( matchLoc.x + templ.cols , matchLoc.y + templ.rows ), Scalar::all(0), 2, 8, 0 );
 
-  imshow( "pattern detection", result );
-
+  imshow( "pattern detection", img_display );
+  
   return;
 }
 
@@ -72,102 +112,100 @@ void pitch_mask(const Mat& source_img, Mat& mask){
   imshow("tmpmask",mask);
 }
 
-void process(const char* imsname, const char* templname, int th){
-  for(int i = 0; i< 16; i++)
-  {
-  Mat img = imread("data/" + imageNames[i], 1);
-  Mat img_gray;
-  Mat mask;
-  int cols = img.cols; 
-  int rows = img.rows; 
+void process(const char* imsname, int th){
 
-  pitch_mask(img, mask);
+  for(int i = 0; i< 1; i++)
+    {
+      Mat img = imread(imsname, 1);
+      Mat img_gray;
+      Mat mask;
+      int cols = img.cols; 
+      int rows = img.rows; 
 
-  if(img.empty()){
-    cerr << "can not open " << imsname << endl;
-  }
+      pitch_mask(img, mask);
+
+      if(img.empty()){
+	cerr << "can not open " << imsname << endl;
+      }
     
-  imshow("source", img);
-  cvtColor( img, img_gray, CV_BGR2GRAY );
+      imshow("source", img);
 
+      cvtColor( img, img_gray, CV_BGR2GRAY );
   
-  for (int i = 0; i<rows; i++){
-    for(int j = 0; j<cols; j++){
-      img_gray.at<uchar>(i,j) = (mask.at<uchar>(i,j)==255 ) ? img_gray.at<uchar>(i,j) : 0;
-      int mean = 0;
-      for(int k = -5;k<6;k++)
-	{
-	  for(int l = -5;l<6;l++)
-	    {	
+      for (int i = 0; i<rows; i++){
+	for(int j = 0; j<cols; j++){
+	  img_gray.at<uchar>(i,j) = (mask.at<uchar>(i,j)==255 ) ? img_gray.at<uchar>(i,j) : 0;
+	  int mean = 0;
+	  for(int k = -5;k<6;k++)
+	    {
+	      for(int l = -5;l<6;l++)
+		{	
 		  int a = (i+k);
 		  int b = (j+l);
 		  if( (a >= 0) && (b >= 0) && (a < rows) && (b < cols) )
 		    {
 		      mean += img_gray.at<uchar>(a,b);
 		    }
+		}
 	    }
+	  mean = mean/121;
+	  if(img_gray.at<uchar>(i,j)>mean+50 && mask.at<uchar>(i,j)==255){
+	    img_gray.at<uchar>(i,j) = 255;
+	  }
+	  else{
+	    img_gray.at<uchar>(i,j) = 0;
+	  }
 	}
-      mean = mean/121;
-      if(img_gray.at<uchar>(i,j)>mean+50 && mask.at<uchar>(i,j)==255){
-	img_gray.at<uchar>(i,j) = 255;
       }
-      else{
-	img_gray.at<uchar>(i,j) = 0;
-	}
-	}
-}
 
     
-  imshow("source+mask", img_gray);
+      imshow("source+mask", img_gray);
 
-  Mat edges;
+      Mat edges;
 
-  Canny( img_gray, edges, 50, 200, 3 );
-  vector<Vec4i> p_lines;
+      Canny( img_gray, edges, 50, 200, 3 );
+      vector<Vec4i> p_lines;
 
-  Mat houghLines(rows, cols, 0, Scalar(0)); 
-  // cvtColor( edges, houghLines, COLOR_GRAY2BGR );
+      Mat houghLines(rows, cols, 0, Scalar(0)); 
 
-  /// Use Probabilistic Hough Transform
-  HoughLinesP( edges, p_lines, 1, CV_PI/180, th, 30, 10 );
+      /// Use Probabilistic Hough Transform
+      HoughLinesP( edges, p_lines, 1, CV_PI/180, th, 30, 10 );
 
-  /// Show the result
-  for( size_t i = 0; i < p_lines.size(); i++ )
-     {
-       Vec4i l = p_lines[i];
-       line( houghLines, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,255,255), 3, CV_AA);
-     }
+      /// Show the result
+      for( size_t i = 0; i < p_lines.size(); i++ )
+	{
+	  Vec4i l = p_lines[i];
+	  line( houghLines, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,255,255), 10, CV_AA);
+	}
 
-  dilate(houghLines,houghLines, getStructuringElement(MORPH_ELLIPSE, Size(10,10)));  
-  erode(houghLines,houghLines , getStructuringElement(MORPH_ELLIPSE, Size(10,10))); //might be useful...
+      //dilate(houghLines,houghLines, getStructuringElement(MORPH_ELLIPSE, Size(10,10)));  
+      //erode(houghLines,houghLines , getStructuringElement(MORPH_ELLIPSE, Size(10,10))); //might be useful...
   
  
 
-  imshow( "lignes detectees", houghLines );
+      imshow( "lignes detectees", houghLines );
   
-  Mat templ = imread(templname,0);
+      matchingMethod(0,houghLines);
+      imwrite("houghLines164.png", houghLines);
   
-  matchingMethod(0,houghLines,templ);
-  imwrite("houghLines164.png",houghLines);
-  
-  waitKey();
-}
+      waitKey();
+    }
      
 }
 
 void usage(const char *s){
   
-  cerr << "Usage: " << s << " imsname template th" << endl;
+  cerr << "Usage: " << s << " imsname th" << endl;
   exit(EXIT_FAILURE);
 
 }
 
-#define param 3
+#define param 2
 int main( int argc, char** argv){
 
   if(argc != (param+1))
     usage(argv[0]);
-  process(argv[1],argv[2],atoi(argv[3]));
+  process(argv[1],atoi(argv[2]));
   return EXIT_SUCCESS;
 
 }
